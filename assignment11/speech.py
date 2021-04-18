b65276d8e2d108b20303450e354f109a0b0e1a06
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-04-18 10:17:45 -0700

    sketch of speech.py

--- assignment11/speech.py
+++ assignment11/speech.py
@@ -1,4 +1,6 @@
 import string
+import os
+import speech_recognition as sr
 
 class Speech:
     def __init__(self):
@@ -19,11 +21,28 @@ class Speech:
                 self.original[idx].append(word)
             self.original.append([])
             idx += 1
-        print(self.original)
 
 
     def conv_audio(self, inDir):
-        pass
+        audio_files_to_text = [[] for _ in range(25)]
+        r = sr.Recognizer()
+        # convert to string
+        for file in os.listdir(inDir):
+            tens_digit = 0
+            if file[-2].isnumeric():
+                tens_digit = (ord(file[-2]) - 48) * 10
+            ones_digit = ord(file[-1]) - 48
+            message = r.recognize_google(file)
+            audio_files_to_text[tens_digit + ones_digit].append(message)
+        # parse
+        for idx,  line in enumerate(audio_files_to_text):
+            punct = string.punctuation
+            punct += '’'
+            for c in punct:
+                line = line.replace(c, '')
+            for word in line.split():
+                self.recognized[idx].append(word)
+            self.recognized.append([])
 
     def comp_string(self):
         pass
