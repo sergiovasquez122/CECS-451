d192953013792fc7a9888d6dff35fe8166ad3b91
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-04-18 09:05:54 -0700

    initialized project

---
+++ assignment11/speech.py
@@ -0,0 +1,17 @@
+class Speech:
+    def __init__(self):
+        self.original = []
+        self.recognized = []
+        self.distances = []
+
+    def read_original(self, inFile):
+        pass
+
+    def conv_audio(self, inDir):
+        pass
+
+    def comp_string(self):
+        pass
+
+if __name__ == '__main__':
+    pass
